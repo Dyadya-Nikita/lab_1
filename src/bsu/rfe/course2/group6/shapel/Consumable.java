8b21522867e54b0ea65ae136c89b4244c3106e93
Nikita <shapelnikita23125@g,ail.com>
2024-12-15 23:57:43 +0300

    Обновлён .gitignore: оставлены только src и .gitignore

--- src/bsu/rfe/course2/group6/shapel/Consumable.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.course2.group6.shapel;
-
-public interface Consumable {
-    public abstract void consume();
-}
--- src/bsu/rfe/course2/group6/shapel/Nutritious.java
+++
@@ -1,5 +0,0 @@
-package bsu.rfe.course2.group6.shapel;
-
-public interface Nutritious {
-    public abstract double calculate_calories();
-}
